5642f42113a74994ac5c7f4eac500d00185e9ecb
Samantha <131906442+Flying-S-Ranch@users.noreply.github.com>
2023-07-15 20:08:02 -0500

    adds update and delete routes to thoughtController.js file

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -23,6 +23,7 @@ module.exports = {
             res.status(500).json(err)
         }
     },
+    // The following route is adapted from the example in module 18, activity 26, appController.js file
     async createThought(req, res) {
         try {
             const thought = await Thought.create(req.body);
@@ -30,11 +31,58 @@ module.exports = {
                 { _id: req.body.userId },
                 { $addToSet: { thoughts: thought._id } },
                 { new: true }
-            )
+            );
+
+            if (!user) {
+                return res.status(404).json({
+                    message: 'Thought added without associated user, no user found with this id'
+                })
+            }
             res.json(thought);
         } catch (err) {
             console.log(err);
             return res.status(500).json(err);
         }
+    },
+    async updateThought(req, res) {
+        try {
+            const thought = await Thought.findOneAndUpdate(
+                { _id: req.params.thoughtId},
+                { $set: req.body },
+                { new: true}
+            );
+            if (!thought) {
+                return res.status(404).json({ message: 'No thought found with this id'})
+            }
+            res.json(thought);
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
+    },
+    async deleteThought(req, res) {
+        try {
+            const thought = await Thought.findOneAndRemove({ _id: req.params.thoughtId });
+    
+            if (!application) {
+                return res.status(404).json({ message: 'no thought found with this id' });
+            }
+    
+            const user = await User.findOneAndUpdate(
+                { applications: req.params.thoughtId },
+                { $pull: { thoughts: req.params.thoughtId } },
+                { new: true }
+            );
+    
+            if (!user) {
+                return res.status(404).json({
+                    message: 'thought deleted, but no user with this id'
+                });
+            }
+            res.json({ message: 'thought deleted' });
+
+        } catch (err) {
+            res.status(500).json(err)
+        } 
     }
 }
